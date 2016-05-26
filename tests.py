01240774d2930ff05e1d3f179a33ab2f9b6c1528
myshanes12 <shane@hex.com>
2016-05-26 17:04:50 +0800

    hello and helloworld

--- tests.py
+++ tests.py
@@ -1,12 +1,12 @@
 from mockito import mock, verify
 import unittest
 
-from helloworld import helloworld
+from hello import hellow
 
 class HelloWorldTest(unittest.TestCase):
     def test_should_issue_hello_world_message(self):
         out = mock()
 
-        helloworld(out)
+        hello(out)
 
         verify(out).write("Hello world of Python\n")
