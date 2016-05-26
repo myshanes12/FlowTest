f73e8cd200c4fd30165236cbda17cb13d372fc69
myshanes12 <shane@hex.com>
2016-05-26 15:23:11 +0800

    Deb package structure

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,39 @@
+import thread
+import time
+import os
 import sys
+from sys import argv
+import signal
 
-def helloworld(out):
-    out.write("Hello world of Python\n")
+
+class Gracefulexit:
+    kill_now=False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+    def exit_gracefully(self,signum, frame):
+        print'do something'
+        self.kill_now = True
+
+def ThreadFunc():
+    while(True):
+        time.sleep(2)
+        print'Im alive'
+
+cmd = argv
+if(len(cmd)>1): 
+    print 'Start with: '
+    print cmd[1]
+else:
+    print 'Start'
+
+thread.start_new_thread(ThreadFunc,())
+
+killer = Gracefulexit()
+while(True):
+    print'Main Thread'
+    if killer.kill_now:
+       break
+    time.sleep(1)
+print'exit gracefully'
+
